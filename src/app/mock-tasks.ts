00b43c12d4c201b2092348a602075ea86544863a
Banadikt <banadikt10@gmail.com>
2022-06-26 18:24:43 +0100

    first commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import { Task } from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Doctor's Appointment",
+        day: 'May 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: "Doctor's Appointment",
+        day: 'May 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: "Mechanic Appointment",
+        day: 'May 7th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 4,
+        text: "Doctor's Appointment",
+        day: 'May 5th at 2:30pm',
+        reminder: false,
+    },
+]
